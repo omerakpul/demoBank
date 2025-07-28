23e78eecd83b53431b2bde7c5dfb73807691bb01
Ömer Emre Akpul <omerakpul38@gmail.com>
2025-07-28 17:46:41 +0300

    Added loginscreen to main activity

--- app/src/main/java/com/vb/demobankapp/MainActivity.kt
+++ app/src/main/java/com/vb/demobankapp/MainActivity.kt
@@ -3,6 +3,7 @@ package com.vb.demobankapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.vb.demobankapp.presentation.ui.auth.login.LoginScreen
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -10,7 +11,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
-            }
+            LoginScreen(
+                onBackClick = {
+                    // Geri git
+                },
+                onContinueClick = { phoneNumber ->
+                    // OTP ekranına git
+                    println("Phone: $phoneNumber - OTP ekranına git")
+                }
+            )
         }
     }
+}
