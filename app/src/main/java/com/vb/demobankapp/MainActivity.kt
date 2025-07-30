2f0ddf06c5533835471f07059e115cfa9deafb9c
Ömer Emre Akpul <omerakpul38@gmail.com>
2025-07-30 17:42:10 +0300

    Get current user ID for HomeScreen and navigate to AddAccountScreen

--- app/src/main/java/com/vb/demobankapp/MainActivity.kt
+++ app/src/main/java/com/vb/demobankapp/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import com.google.firebase.auth.FirebaseAuth
+import com.vb.demobankapp.presentation.ui.account.AddAccountScreen
 import com.vb.demobankapp.presentation.ui.auth.login.LoginScreen
 import com.vb.demobankapp.presentation.ui.auth.otp.OtpScreen
 import com.vb.demobankapp.presentation.ui.auth.register.RegisterScreen
@@ -19,45 +21,41 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            var currentScreen by remember { mutableStateOf("login") }
+            var currentScreen by remember { mutableStateOf("login") } // Başlangıç login
             var phoneNumber by remember { mutableStateOf("") }
 
+            // Anlık kullanıcı ID'si
+            val currentUserId = FirebaseAuth.getInstance().currentUser?.uid ?: ""
+
             when (currentScreen) {
-                "login" -> {
-                    LoginScreen(
-                        onBackClick = { },
-                        onContinueClick = { phone ->
-                            phoneNumber = phone
-                            currentScreen = "otp"
-                        }
-                    )
-                }
-                "otp" -> {
-                    OtpScreen(
-                        phoneNumber = phoneNumber,
-                        onBackClick = { currentScreen = "login" },
-                        onVerifySuccess = { currentScreen = "home" },
-                        onUserNotFound = { currentScreen = "register" }
-                    )
-                }
-                "register" -> {
-                    RegisterScreen(
-                        phoneNumber = phoneNumber,
-                        onBackClick = { currentScreen = "otp" },
-                        onRegisterSuccess = { currentScreen = "home" }
-                    )
-                }
-                "home" -> {
-                    HomeScreen(
-                        userId = "123",
-                        onAddAccountClick = {
-                            // Account ekleme işlemi burada yapılacak
-                            // Şimdilik boş bırakıyoruz, HomeViewModel'de handle edilecek
-                        },
-                        onTransferClick = { },
-                        onCurrencyClick = { }
-                    )
-                }
+                "login" -> LoginScreen(
+                    onBackClick = { /* Geri git */ },
+                    onContinueClick = { phone ->
+                        phoneNumber = phone
+                        currentScreen = "otp"
+                    }
+                )
+                "otp" -> OtpScreen(
+                    phoneNumber = phoneNumber,
+                    onBackClick = { currentScreen = "login" },
+                    onVerifySuccess = { currentScreen = "home" },
+                    onUserNotFound = { currentScreen = "register" }
+                )
+                "register" -> RegisterScreen(
+                    phoneNumber = phoneNumber,
+                    onBackClick = { currentScreen = "otp" },
+                    onRegisterSuccess = { currentScreen = "home" }
+                )
+                "home" -> HomeScreen(
+                    userId = currentUserId,
+                    onAddAccountClick = { currentScreen = "addAccount" },
+                    onTransferClick = { /* Transfer ekranına git */ },
+                    onCurrencyClick = { /* Currency ekranına git */ }
+                )
+                "addAccount" -> AddAccountScreen(
+                    onBackClick = { currentScreen = "home" },
+                    onAddClick = { /* ViewModel kendi işini yapıyor */ }
+                )
             }
         }
     }
